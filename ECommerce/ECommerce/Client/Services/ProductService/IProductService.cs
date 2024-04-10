cf98f313a61736d590b693dd8be06f99320ed605
ingan <ingan@LAPTOP-NU8F1P04>
2024-04-10 13:07:44 +0200

    Implemented searching functionality (added new component), added search suggestions and change loading product method

--- ECommerce/ECommerce/Client/Services/ProductService/IProductService.cs
+++ ECommerce/ECommerce/Client/Services/ProductService/IProductService.cs
@@ -4,9 +4,13 @@
     {
         //event- change product list
         event Action ProductsChanged;
+        string Message { get; set; }
         List<Product> Products { get; set; }
         Task GetProducts(string? categoryUrl = null);
 
         Task<ServiceResponse<Product>> GetProduct(int productId);
+
+        Task SearchProducts(string searchText);
+        Task<List<string>> GetProductSearchSuggestions(string searchText);
     }
 }
--- ECommerce/ECommerce/Client/Services/ProductService/ProductService.cs
+++ ECommerce/ECommerce/Client/Services/ProductService/ProductService.cs
@@ -11,6 +11,7 @@
         }
 
         public List<Product> Products { get; set; } = new List<Product>();
+        public string Message { get; set; } = "Loading products...";
 
         public async Task<ServiceResponse<Product>> GetProduct(int productId)
         {
@@ -29,6 +30,22 @@
                 Products = result.Data;
             //invoke event- triggered an event informing about product change
             ProductsChanged.Invoke();
+        }
+        public async Task<List<string>> GetProductSearchSuggestions(string searchText)
+        {
+            var result = await _http
+                .GetFromJsonAsync<ServiceResponse<List<string>>>($"api/product/searchsuggestions/{searchText}");
+            return result.Data;
+        }
+
+        public async Task SearchProducts(string searchText)
+        {
+            var result = await _http
+                 .GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
+            if (result != null && result.Data != null)
+                Products = result.Data;
+            if (Products.Count == 0) Message = "Product is not available";
+            ProductsChanged?.Invoke();
         }
     }
 }
