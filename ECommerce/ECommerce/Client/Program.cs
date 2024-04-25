e12fc6f96435b767947bed858a5eb13938de6fb3
ingan <ingan@LAPTOP-NU8F1P04>
2024-04-25 10:44:27 +0200

    Added local storage to project (installed for client project NuGet package - Blazored.LocalStorage)

--- ECommerce/ECommerce/Client/Program.cs
+++ ECommerce/ECommerce/Client/Program.cs
@@ -2,6 +2,7 @@ global using ECommerce.Shared;
 global using System.Net.Http.Json;
 global using ECommerce.Client.Services.ProductService;
 global using ECommerce.Client.Services.CategoryService;
+using Blazored.LocalStorage;
 using ECommerce.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -9,7 +10,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+//registred localStorage
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 //registered ProductService
 builder.Services.AddScoped<IProductService, ProductService>();
