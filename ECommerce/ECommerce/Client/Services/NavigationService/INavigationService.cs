32cd81872e34aca23c19eca4a2c35f575cec5e31
Adrian Kozieł <118553417+Prispajer@users.noreply.github.com>
2024-05-20 23:52:39 +0200

    Created a unified NavigationService to handle navigation across the entire application, users can now switch between mobile and desktop navigation seamlessly

---
+++ ECommerce/ECommerce/Client/Services/NavigationService/INavigationService.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerce.Client.Services.NavigationService
+{
+    public interface INavigationService
+    {
+        event Action? NavStateChanged;
+        bool NavState { get; set; }
+        void ChangeNavState(bool state);
+    }
+}
---
+++ ECommerce/ECommerce/Client/Services/NavigationService/NavigationService.cs
@@ -0,0 +1,15 @@
+﻿namespace ECommerce.Client.Services.NavigationService
+{
+    public class NavigationService : INavigationService
+    {
+        public event Action? NavStateChanged;
+        public bool NavState { get; set; } = false;
+
+        public void ChangeNavState(bool state)
+        {
+            NavState = state;
+            NavStateChanged?.Invoke();
+        }
+    }
+}
+
