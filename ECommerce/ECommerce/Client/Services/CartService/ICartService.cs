d392662ce6b9bda5df0f018ad33dfd6bdcc17d99
Adrian Kozieł <118553417+Prispajer@users.noreply.github.com>
2025-03-13 23:41:43 +0100

    Fixed data display problem

--- ECommerce/ECommerce/Client/Services/CartService/ICartService.cs
+++ ECommerce/ECommerce/Client/Services/CartService/ICartService.cs
@@ -4,6 +4,7 @@ namespace ECommerce.Client.Services.CartService
 {
     public interface ICartService
     {
+        event Action? OnChange;
         Task AddToCart(CartItem cartItem);
         Task<List<CartItem>> GetCartItems();
         Task<List<CartProductResponse>> GetCartProducts();
